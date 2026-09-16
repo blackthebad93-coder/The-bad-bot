21ca8950f0e94e8e5622efee99885b1a8cd1838e
blackthebad93-coder <blackthebad93@gmail.com>
2026-09-16 22:29:36 +0100

    Create messageStyle.js

---
+++ utils/messageStyle.js
@@ -0,0 +1,18 @@
+import fs from "fs"
+import stylizedChar from "./fancy.js"
+
+export default function stylizedCardMessage(text) {
+  return {
+    text: stylizedChar(text),
+    contextInfo: {
+      externalAdReply: {
+        title: "The bad bot",
+        body: "𓆩 The bad bot 𓆪",
+        thumbnail: fs.readFileSync("./database/DigiX.jpg"),
+        sourceUrl: "https://whatsapp.com",
+        mediaType: 1,
+        renderLargerThumbnail: false
+      }
+    }
+  }
+}
